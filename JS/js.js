6636150bdfa0112604d626b85b104795cfe50048
yukasan <yuka11on7@gmail.com>
2022-01-03 12:24:43 +0900

    main

--- JS/js.js
+++ JS/js.js
@@ -41,3 +41,20 @@ target.addEventListener('click', () => {
   const nav = document.getElementById("nav");
   nav.classList.toggle('in');
 });
+
+
+//モーダルウィンドウ//
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const modal = document.getElementById('modal');
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBtn") {
+    modal.style.display = 'none';
+  }
+});
